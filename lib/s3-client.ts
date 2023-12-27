f3cfe05f3fa53a10a60c64b12149043fa179355e
Navin Avadhani <knavinavadhani@gmail.com>
2023-12-27 23:45:54 +0530

    Added S3 helpers.

--- lib/s3-client.ts
+++ lib/s3-client.ts
@@ -1,9 +1,45 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export const s3Client = new S3Client({
+const BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const s3Client = new S3Client({
   region: process.env.S3_BUCKET_REGION,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY!,
     secretAccessKey: process.env.S3_SECRET_KEY!,
   },
 });
+
+export async function getS3SignedURL(key: string) {
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+  });
+  const signedURL = await getSignedUrl(s3Client, command, {
+    expiresIn: 60,
+  });
+  return signedURL;
+}
+
+export async function getS3UploadURL(key: string, contentType: string) {
+  const command = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+    ContentType: contentType,
+  });
+  const signedURL = await getSignedUrl(s3Client, command);
+  return signedURL;
+}
+
+export async function deleteS3Object(key: string) {
+  const command = new DeleteObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+  });
+  await s3Client.send(command);
+}
