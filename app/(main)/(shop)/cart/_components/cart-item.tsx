0975afb376869f15f39f328075025e838641d16e
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-09 19:15:31 +0530

    Minor styling refactor.

--- app/(main)/(shop)/cart/_components/cart-item.tsx
+++ app/(main)/(shop)/cart/_components/cart-item.tsx
@@ -40,7 +40,7 @@ export function CartItem({
           <div className="relative hidden aspect-square w-[150px] md:block lg:w-[200px]">
             <Image fill src={imageUrl} alt={`Image for ${productName}`} />
           </div>
-          <div className="flex flex-col justify-between gap-y-4">
+          <div className="flex flex-col justify-between gap-y-4 py-2">
             <div>
               <h3 className="line-clamp-1 text-lg font-medium">
                 {productName}
