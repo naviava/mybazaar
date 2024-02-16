20bb1cb7053b9e2594dab7b39be81c209e7b07fd
Navin Avadhani <knavinavadhani@gmail.com>
2024-02-16 18:23:01 +0530

    Refactor product selection layout.

--- app/(main)/(shop)/products/[productId]/product-selection.tsx
+++ app/(main)/(shop)/products/[productId]/product-selection.tsx
@@ -4,15 +4,15 @@ interface IProps {}
 
 export function ProductSelection({}: IProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-      <div className="grid grid-cols-1 lg:col-span-2 lg:grid-cols-2">
-        <div className="grid grid-cols-2">
+    <div className="flex items-center">
+      <div className="flex-1 space-y-2">
+        <div className="flex items-center gap-x-6">
           <p className="font-medium">COLOR</p>
           <ColorPicker />
         </div>
         <div>Quantity</div>
       </div>
-      <div>
+      <div className="flex flex-1 flex-col items-end justify-center">
         <div>Price</div>
         <div>Button</div>
       </div>
